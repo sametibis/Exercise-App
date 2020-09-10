3a9603e4d06668bdb6df3ff4669ba9f30a28f2d3
sametibis <sametibs.dev@gmail.com>
2020-09-11 00:20:43 +0300

    Search Exercise by Username

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -2,58 +2,105 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const Exercise = props => (
+const Exercise = (props) => (
   <tr>
     <td>{props.exercise.username}</td>
     <td>{props.exercise.description}</td>
     <td>{props.exercise.duration}</td>
-    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
     <td>
-      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+      <Link to={'/edit/' + props.exercise._id}>edit</Link> |{' '}
+      <a
+        href='#'
+        onClick={() => {
+          props.deleteExercise(props.exercise._id);
+        }}
+      >
+        delete
+      </a>
     </td>
   </tr>
-)
+);
 
 export default class ExercisesList extends Component {
   constructor(props) {
     super(props);
 
-    this.deleteExercise = this.deleteExercise.bind(this)
+    this.deleteExercise = this.deleteExercise.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
 
-    this.state = {exercises: []};
+    this.state = { exercises: [], username: '' };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+    };
+
+    this.setState({
+      exercises: this.state.exercises.filter(
+        (exercise) => exercise.username === this.state.username
+      ),
+    });
+  }
+
+  onChange(e) {
+    e.preventDefault();
+
+    this.setState({
+      username: e.target.value,
+    });
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/exercises/')
-      .then(response => {
-        this.setState({ exercises: response.data })
+    axios
+      .get('http://localhost:5000/exercises/')
+      .then((response) => {
+        this.setState({ exercises: response.data });
       })
       .catch((error) => {
         console.log(error);
-      })
+      });
   }
 
   deleteExercise(id) {
-    axios.delete('http://localhost:5000/exercises/'+id)
-      .then(response => { console.log(response.data)});
+    axios.delete('http://localhost:5000/exercises/' + id).then((response) => {
+      console.log(response.data);
+    });
 
     this.setState({
-      exercises: this.state.exercises.filter(el => el._id !== id)
-    })
+      exercises: this.state.exercises.filter((el) => el._id !== id),
+    });
   }
 
   exerciseList() {
-    return this.state.exercises.map(currentexercise => {
-      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
-    })
+    return this.state.exercises.map((currentexercise) => {
+      return (
+        <Exercise
+          exercise={currentexercise}
+          deleteExercise={this.deleteExercise}
+          key={currentexercise._id}
+        />
+      );
+    });
   }
 
   render() {
     return (
       <div>
         <h3>Logged Exercises</h3>
-        <table className="table">
-          <thead className="thead-light">
+        <form onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            value={this.state.username}
+            onChange={this.onChange}
+          />
+        </form>
+        <table className='table'>
+          <thead className='thead-light'>
             <tr>
               <th>Username</th>
               <th>Description</th>
@@ -62,11 +109,9 @@ export default class ExercisesList extends Component {
               <th>Actions</th>
             </tr>
           </thead>
-          <tbody>
-            { this.exerciseList() }
-          </tbody>
+          <tbody>{this.exerciseList()}</tbody>
         </table>
       </div>
-    )
+    );
   }
 }
